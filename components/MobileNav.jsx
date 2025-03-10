c012fb2806eaa6892d8441748d1c84d52df086f2
Azad Chandio <chandiok96@gmail.com>
2025-03-10 08:23:43 +0500

    Enhance MobileNav component with state management for menu interaction

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -4,6 +4,7 @@ import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { CiMenuFries } from "react-icons/ci";
+import { useState } from "react";
 
 const links = [
   {
@@ -27,17 +28,20 @@ const links = [
     path: "/contact",
   },
 ];
+
 const MobileNav = () => {
   const pathname = usePathname();
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <Sheet>
+    <Sheet open={isOpen} onOpenChange={setIsOpen}>
       <SheetTrigger className="flex justify-center items-center">
         <CiMenuFries className="text-[32px] text-accent" />
       </SheetTrigger>
       <SheetContent className="flex flex-col">
         {/* {logo} */}
         <div className="mt-32 mb-32 text-center text-2xl">
-          <Link href="/">
+          <Link href="/" onClick={() => setIsOpen(false)}>
             <h1 className="text-4xl font-semibold">
               Azad <span className="text-accent">.</span>
             </h1>
@@ -50,7 +54,10 @@ const MobileNav = () => {
               <Link
                 href={link.path}
                 key={index}
-                className={`${link.path === pathname && "text-accent border-b-2 border-accent   "}text-xl capitalize hover:text-accent transition-all`}
+                onClick={() => setIsOpen(false)}
+                className={`${
+                  link.path === pathname && "text-accent border-b-2 border-accent"
+                }text-xl capitalize hover:text-accent transition-all`}
               >
                 {link.name}
               </Link>

e16ab815b880baa15807ee742511f8a28c69de6d
Azad Chandio <chandiok96@gmail.com>
2025-07-01 20:58:07 +0500

    Moved  project images to Supabase Storage Configured next.config.js to load external images securely

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['tjvitedcsgubvbjuiopl.supabase.co'], // ✅ no https://
+  },
+};
 
 export default nextConfig;
